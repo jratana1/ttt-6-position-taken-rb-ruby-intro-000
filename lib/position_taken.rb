0ca8acf1c214a5b90d616e2c5bcfd31375b3ae0b
John Ratanaprasatporn <jratana1@gmail.com>
2020-09-02 20:08:51 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board[index - 1] == " "
+  if board[index - 1] == " " || board[index - 1] == ""
     return false
+elsif type(board[index - 1]) == nil
+  return false
   end
 end
